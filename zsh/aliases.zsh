1809a7b94a546a3120db4221b8830ec9d21a0ced
Daniel Turner <daniel.turner@zip.co>
2021-05-18 17:23:38 +1000

    Enterprise

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,7 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias k='kubectl'
 
 alias dc='docker-compose'
+
+alias nopeskope='sudo launchctl unload /Library/LaunchDaemons/com.netskope.client.auxsvc.plist'
+
+alias yepskope='sudo launchctl load /Library/LaunchDaemons/com.netskope.client.auxsvc.plist'
